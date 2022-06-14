be8f5178ac6ad2d5f20f52dc900aa484ff7fe12d
hyuk0309 <bahngeh@naver.com>
2022-06-14 19:04:09 +0900

    test : github action test용 test 추가

---
+++ src/test/java/prgms/marco/springbooturlshortener/githubaction/SuccessTest.java
@@ -0,0 +1,12 @@
+package prgms.marco.springbooturlshortener.githubaction;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SuccessTest {
+
+    @Test
+    void successTest() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
